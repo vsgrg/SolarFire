75946fa55e144177ab64efc91b042c48b2d794fa
BishalGrg <vsgrg10@gmail.com>
2020-08-06 12:37:28 +0530

    added requirements.txt

--- collectdata/urls.py
+++ collectdata/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     
     path('',views.home,name='home'),
-    path('login', views.login_request, name="login"),
+    path('login/', views.login_request, name="login"),
     path("details/", views.Details, name="details"),
     path('add_form/',views.add_form,name='add_form'),
-    path("logout", views.logout_request, name="logout"),
+    path("logout/", views.logout_request, name="logout"),
     
 ]
